f7f35a1fee54179d1777ea430866208dd8a6522a
David-Carrasco-Vidaurre <56795495+david-carrasco-vidaurre@users.noreply.github.com>
2019-11-23 23:24:19 -0500

    se crea el lote.bat

---
+++ lote.bat
@@ -0,0 +1,63 @@
+python validadores\validador_mientras01.py "David" "Carrasco" "davidjsmell@gmail.com" "76533186" 20
+
+python validadores\validador_mientras02.py "David"  76533186 20 "21/11/2019" "tumbes_chiclayo" 25
+
+python validadores\validador_mientras03.py "David" 935903932 "Ing electronica" "21/11/2019" "Ordinario" 200
+
+python validadores\validador_mientras04.py "David" 17 "25 sep" "10pm" "Mi casa"
+
+python validadores\validador_mientras05.py 15 10 5
+
+python repetitvas_para\repetitivas_para01.py 3
+
+python repetitvas_para\repetitivas_para02.py 5
+
+python repetitvas_para\repetitivas_para03.py 2 4
+
+python repetitvas_para\repetitivas_para04.py 3
+
+python repetitvas_para\repetitivas_para05.py 5
+
+python repetitivas_interacion\repetitivas_interacion01.py "David" 10
+
+python repetitivas_interacion\repetitivas_interacion02.py 17
+
+python repetitivas_interacion\repetitivas_interacion03.py 17
+
+python repetitivas_interacion\repetitivas_interacion04.py "dcarrascov@unprg.edu.pe"
+
+python repetitivas_interacion\repetitivas_interacion05.py 15
+
+python repetitivas_intercion_rango\repetitivas_rango01.py "ABGFCED"
+
+python repetitivas_intercion_rango\repetitivas_rango02.py "PVOI"
+
+python repetitivas_intercion_rango\repetitivas_rango03.py "MPNQ"
+
+python repetitivas_intercion_rango\repetitivas_rango04.py "UNPRG"
+
+python repetitivas_intercion_rango\repetitivas_rango05.py "DJCV"
+
+python Validadores/Validador6.py 15
+python Validadores/Validador7.py 20 35
+python Validadores/Validador8.py 0 100
+python Validadores/Validador9.py agua fuego tierra aire
+python Validadores/Validador10.py lunes martes miercoles jueves viernes
+
+python repetitivas para/recorrido6.py 300
+python repetitivas para/recorrido7.py 600
+python repetitivas para/recorrido8.py 10
+python repetitivas para/recorrido9.py 150
+python repetitivas para/recorrido10.py 30
+
+python iteracion/iteracion6.py juan
+python iteracion/iteracion7.py 145987
+python iteracion/iteracion8.py 154
+python iteracion/iteracion9.py esternocleidomastoideo
+python iteracion/iteracion10.py 154998515674
+
+python decodificadores/decodificador6.py hola como_estas  bien  mal
+python decodificadores/decodificador7.py Hay_clases Estoy_en_aula Ven_al_laboratorio Vamos_a_comer Adios
+python decodificadores/decodificador8.py Te_veo_luego Tengo_sed Tengo_hambre Vamos_al_cine Tengo_tarea Salida
+python decodificadores/decodificador9.py Hola Estoy_bien Estoy_mal Me_voy Voy_a_dormir Tengo_examen Voy_a_banarme Voy_a_correr Tengo_Fiesta Vamos_a_nadar Te_odio Te_amo
+python decodificadores/decodificador10.py Yo Soy De La Pedro
