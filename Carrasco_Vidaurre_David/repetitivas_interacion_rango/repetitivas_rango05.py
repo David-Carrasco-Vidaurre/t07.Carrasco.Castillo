372af89509b916b3c119a9db20582867ef27ac7b
David-Carrasco-Vidaurre <56795495+david-carrasco-vidaurre@users.noreply.github.com>
2019-11-23 23:07:38 -0500

    se crea el programa repetitivas_rango05.py

---
+++ Carrasco_Vidaurre_David/repetitivas_interacion_rango/repetitivas_rango05.py
@@ -0,0 +1,17 @@
+#estaciones "DJCV"
+import os
+palabra=os.sys.argv[1]
+
+
+
+for letra_nombre in palabra:
+    if(letra_nombre=="D"):
+        print("David")
+    if(letra_nombre=="J"):
+        print("Josmell")
+    if(letra_nombre=="C"):
+        print("Carrasco")
+    if(letra_nombre=="V"):
+        print("Vidaurre")
+
+    #fin bucle
